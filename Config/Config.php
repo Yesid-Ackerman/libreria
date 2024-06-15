36af42a5d6a630e8d8b1f3a84d95c6c39cac6914
Yesid-Ackerman~ <deironyesithym21@gmail.com>
2024-06-15 14:32:29 -0500

    Result Library Final

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL = "http://localhost/libreria/";
+    const HOST = "localhost";
+    const BD = "biblioteca";
+    const DB_USER = "root";
+    const PASS = "";
+    const CHARSET = "charset=utf8";
+?>
